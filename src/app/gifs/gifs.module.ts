0a3205ff8c93c2d98aa4c309f6a59bdd1fd6b6c8
Jair Deyner <jairdeyner730791@gmail.com>
2023-03-20 12:46:07 -0500

    image loader added

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
+  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent, GifsCardComponent],
   exports: [HomePageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 })
 export class GifsModule {}
